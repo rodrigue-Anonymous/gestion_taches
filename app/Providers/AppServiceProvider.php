025fff355bc132061dfff4e8ddea142262ac952f
unknown <rodriguebet@gmail.com>
2024-11-26 16:00:16 +0100

    UDATE

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{    
+    
+    // J'ai decher de dinuee iciiiiiiii
+    //public const HOME = '/'; // Redirige vers l'URL d'accueil au lieu de /home
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
